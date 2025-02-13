613ebf27e2d17efd3b5813dbdb437fd57fb95df4
Emmanuele <>
2025-02-13 23:21:04 +0100

    Added close button in the checkout

--- app/checkout.tsx
+++ app/checkout.tsx
@@ -1,27 +1,36 @@
 import { useLocalSearchParams, useRouter } from 'expo-router';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { WebView } from 'react-native-webview';
 import { LoadingScreen } from '@/components/LoadingScreen';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, SafeAreaView, Text } from 'react-native';
 
 export default function CheckoutPage() {
   const { url } = useLocalSearchParams();
   const router = useRouter();
   const webViewRef = useRef<WebView>(null);
+  const [showCloseButton, setShowCloseButton] = useState(false);
 
   if (!url) {
     return null;
   }
 
   const handleNavigationStateChange = (navState: { url: string }) => {
-    // Handle successful order or cancellation
-    if (navState.url.includes('/thank-you')) {
-      // TODO: Add button to close the page
-    }
+    setShowCloseButton(navState.url.includes('thank_you'));
   };
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+        <Text style={styles.headerTitle}>Checkout</Text>
+            {showCloseButton && (
+              <TouchableOpacity
+                style={styles.closeButton}
+                onPress={() => router.replace('/(tabs)/home')}
+              >
+                <Text style={styles.closeButtonText}>Close</Text>
+              </TouchableOpacity>
+            )}
+        </View>
       <WebView
         ref={webViewRef}
         source={{ uri: decodeURIComponent(url as string) }}
@@ -30,7 +39,7 @@ export default function CheckoutPage() {
         renderLoading={() => <LoadingScreen />}
         style={styles.webview}
       />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -39,7 +48,31 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  header: {
+    height: 75,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingHorizontal: 16,
+    paddingTop: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f0f0f0',
+  },
+  closeButton: {
+    padding: 12,
+  },
   webview: {
     flex: 1,
+  },
+  headerTitle: {
+    flex: 1,
+    fontSize: 20,
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+  closeButtonText: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#000',
   },
 });
