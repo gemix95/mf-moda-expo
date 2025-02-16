a00c2668aed38977e6225626d55c7acb664e06a3
Emmanuele <>
2025-02-16 18:18:13 +0100

    Added title product detail

--- app/(tabs)/search/product.tsx
+++ app/(tabs)/search/product.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { useProductStore } from '@/types/productStore';
 import { ProductView } from '@/components/ProductView';
+import { Stack } from 'expo-router';
 
 export default function ProductScreen() {
   const selectedProduct = useProductStore((state) => state.selectedProduct);
 
   if (!selectedProduct) return null;
 
-  return <ProductView product={selectedProduct} />;
+  return (
+    <>
+      <Stack.Screen 
+        options={{
+          title: selectedProduct.brand
+        }} 
+      />
+      <ProductView product={selectedProduct} />
+    </>
+  );
 }
